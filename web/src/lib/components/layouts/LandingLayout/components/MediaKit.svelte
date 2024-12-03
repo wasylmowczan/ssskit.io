ad3548b81bb97e579ed3043f83ee4f681892df0f
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-12-03 08:01:54 +0100

    Add possibility to download logos by context menu to i18n version

---
+++ web/src/lib/components/layouts/LandingLayout/components/MediaKit.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import * as ContextMenu from '$lib/components/ui/context-menu';
+	import { ImageDown, Download } from 'lucide-svelte';
+
+	function downloadSvg(filename: string) {
+		const link = document.createElement('a');
+		link.href = `/${filename}`;
+		link.download = filename;
+		link.click();
+	}
+</script>
+
+<ContextMenu.Root>
+	<ContextMenu.Trigger><slot /></ContextMenu.Trigger>
+	<ContextMenu.Content>
+		<ContextMenu.Item on:click={() => downloadSvg('logo.svg')}>
+			<ImageDown class="w-4 h-4 mr-2" /> Download Logo SVG
+		</ContextMenu.Item>
+		<ContextMenu.Item on:click={() => downloadSvg('seo-image.png')}>
+			<Download class="w-4 h-4 mr-2" /> Download SEO Image
+		</ContextMenu.Item>
+		<ContextMenu.Item on:click={() => downloadSvg('favicon.png')}>
+			<Download class="w-4 h-4 mr-2" /> Download Favicon
+		</ContextMenu.Item>
+	</ContextMenu.Content>
+</ContextMenu.Root>
